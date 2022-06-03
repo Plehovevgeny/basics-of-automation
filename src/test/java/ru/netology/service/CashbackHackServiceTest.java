ecce2faee45175a12cc50c455f67ffb3b5a4034e
Plehov Evgeny <plehov1992@yandex.ru>
2022-06-03 22:07:27 +0500

    add testng and tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    void shouldReturn0IfBoundary1000() {
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    void shouldReturn100IfBoundary900() {
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    void shouldReturn200IfBoundary1800() {
+
+        int actual = service.remain(1800);
+        int expected = 200;
+
+        assertEquals(actual, expected);
+
+    }
+}
