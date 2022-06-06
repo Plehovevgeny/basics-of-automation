2e5e2e4034f3f42f6334ba2d8d117833e3e8ab80
Plehov Evgeny <plehov1992@yandex.ru>
2022-06-06 18:56:42 +0500

    add tests with junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,39 @@
+package ru.netology.service;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturn0IfBoundary1000() {
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldReturn100IfBoundary900() {
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldReturn200IfBoundary1800() {
+
+        int actual = service.remain(1800);
+        int expected = 200;
+
+        assertEquals(actual, expected);
+
+    }
+}
